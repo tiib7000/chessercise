f1cd4b5883879d946469e1f54a42bc8ee02352cb
Tibi <tiberiu.ovidiu.dumitrescu@gmail.com>
2017-10-10 19:32:08 -0400

    added rook and knight

--- chessercise.py
+++ chessercise.py
@@ -18,20 +18,20 @@ class Position:
 		self.column += 1
 
 	def ne(self):
-		north(self)
-		east(self)
+		self.north()
+		self.east()
 
 	def nw(self):
-		north(self)
-		west(self)
+		self.north()
+		self.west()
 
 	def se(self):
-		south(self)
-		east(self)
+		self.south()
+		self.east()
 
 	def sw(self):
-		south(self)
-		west(self)
+		self.south()
+		self.west()
 
 	def validate(self):
 		return self.line >= 0 and self.line <= 7 and \
@@ -40,26 +40,32 @@ class Position:
 	def __str__(self):
 		return str("Position " + chr(self.line + ord('1')) + " " + chr(self.column+ord('a')))
 
+	@staticmethod
 	def parse(s):
 		return Position(ord(s[1])-ord('1'), ord(s[0])-ord('a'))
-	parse = staticmethod(parse)
 
 class Step:
-	COUNTER_MAX=7
-	COUNTER_MIN=1
-	COUNTER_WILDCARD=0
+	REPEAT_MAX=8
+	REPEAT_MIN=1
+	REPEAT_WILDCARD=0
 
-	def __init__(self, direction, counter):
+	def __init__(self, direction, repeat):
 		self.direction = direction
-		self.counter = counter
+		self.repeat = repeat
 
 	def apply(self, positions):
 		newPositions = []
 		for p in positions:
 			newPosition = Position(p.line, p.column)
-			for i in range(self.counter):
-				self.direction(newPosition)
-			newPositions.append(newPosition)
+			if self.repeat >= Step.REPEAT_MIN and self.repeat <= Step.REPEAT_MAX:
+				for i in range(self.repeat):
+					self.direction(newPosition)
+				newPositions.append(newPosition)
+			elif self.repeat == Step.REPEAT_WILDCARD:
+				for i in range(Step.REPEAT_MIN, Step.REPEAT_MAX):
+					self.direction(newPosition)
+					clone = Position(newPosition.line, newPosition.column)
+					newPositions.append(clone)
 		return newPositions
 
 class Move:
@@ -82,10 +88,13 @@ class Piece:
 	def __init__(self, position):
 		self.position = position
 
+	@staticmethod
 	def factory(type, position):
 		if type == 'Pawn': return Pawn(position)
+		if type == 'Queen': return Queen(position)
+		if type == 'Rook': return Rook(position)
+		if type == 'Knight': return Knight(position)
 		assert 0, "Bad piece type: " + type
-	factory = staticmethod(factory)
 
 
 class Pawn(Piece):
@@ -99,18 +108,46 @@ class Pawn(Piece):
 
 class Queen(Piece):
 	moves = (\
-		Move((Step(Position.north,1),)),\
-		Move((Step(Position.south,1),)),\
-		Move((Step(Position.east,1),)),\
-		Move((Step(Position.west,1),)),\
-		Move((Step(Position.ne,1),)),\
-		Move((Step(Position.se,1),)),\
-		Move((Step(Position.sw,1),)),\
-		Move((Step(Position.nw,1),)),\
+		Move((Step(Position.north,Step.REPEAT_WILDCARD),)),\
+		Move((Step(Position.south,Step.REPEAT_WILDCARD),)),\
+		Move((Step(Position.east,Step.REPEAT_WILDCARD),)),\
+		Move((Step(Position.west,Step.REPEAT_WILDCARD),)),\
+		Move((Step(Position.ne,Step.REPEAT_WILDCARD),)),\
+		Move((Step(Position.se,Step.REPEAT_WILDCARD),)),\
+		Move((Step(Position.sw,Step.REPEAT_WILDCARD),)),\
+		Move((Step(Position.nw,Step.REPEAT_WILDCARD),)),\
 		)
 
 	def __str__(self):
 		return str("Queen " + str(self.position))
+
+
+class Rook(Piece):
+	moves = (\
+		Move((Step(Position.north,Step.REPEAT_WILDCARD),)),\
+		Move((Step(Position.south,Step.REPEAT_WILDCARD),)),\
+		Move((Step(Position.east,Step.REPEAT_WILDCARD),)),\
+		Move((Step(Position.west,Step.REPEAT_WILDCARD),)),\
+		)
+
+	def __str__(self):
+		return str("Rook " + str(self.position))
+
+
+class Knight(Piece):
+	moves = (\
+		Move((Step(Position.north,1),Step(Position.west,2),)),\
+		Move((Step(Position.north,1),Step(Position.east,2),)),\
+		Move((Step(Position.south,1),Step(Position.west,2),)),\
+		Move((Step(Position.south,1),Step(Position.east,2),)),\
+		Move((Step(Position.north,2),Step(Position.west,1),)),\
+		Move((Step(Position.north,2),Step(Position.east,1),)),\
+		Move((Step(Position.south,2),Step(Position.west,1),)),\
+		Move((Step(Position.south,2),Step(Position.east,1),)),\
+		)
+
+	def __str__(self):
+		return str("Knight " + str(self.position))
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-piece", help="Type of chess piece (Queen, Rook, Knight)", required=True)
@@ -119,21 +156,22 @@ parser.parse_args()
 args = parser.parse_args()
 
 pos = Position.parse(args.position)
-print(pos)
+#print(pos)
 
 piece = Piece.factory(args.piece, pos)
-print()
+#print(piece)
 
-pos.north()
-print(pos)
+#pos.north()
+#print(pos)
 
-b = pos.validate()
+#b = pos.validate()
 
-print(b)
+#print(b)
+
 for m in piece.moves:
-	print(m)
-	for s in m.steps:
-		print(s)
+#	print(m)
+#	for s in m.steps:
+#		print(s)
 	positions = m.apply(piece.position)
 	if positions != None:
 		for p in positions:
